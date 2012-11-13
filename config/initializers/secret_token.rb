05242fd6832977737847855b287f4ce13a200fae
Paul Sutcliffe <paul@kosmyka.com>
2012-11-12 23:51:19 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Laposta::Application.config.secret_token = '785ef40bb433886779adf1ee7c4cc95ea52df386eef0ad1aa0e747f6e8dad830ead1576c7d2d9f1c8c1727f279ca87bfe056848cd43b97db29e2394dbde58f38'
