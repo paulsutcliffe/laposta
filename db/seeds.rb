00705a4f4442bd917c5b966316048bedcadf8ac2
Ricardo <marcorcp1@hotmail.com>
2012-11-15 03:06:16 -0500

    Migrating files from Serve to Rails App

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+
+Miembro.delete_all
+Miembro.create(
+  :nombre => 'LOURDES PEÑA',
+  :cargo => 'CEO / Productora General',
+  :correo => 'lourdes@lapost-a.com',
+  :telefono => '986 289 315' )
+
+Miembro.create(
+  :nombre => 'WALTER PEÑAHERRERA',
+  :cargo => 'Director / Post Productor',
+  :correo => 'walter@lapost-a.com',
+  :telefono => '9994 5 9995' )
+
+Miembro.create(
+  :nombre => 'HIROSHI OGA',
+  :cargo => 'Edición y Post Producción',
+  :correo => 'hiroshi@lapost-a.com',
+  :telefono => '986 270 475' )
+
+Miembro.create(
+  :nombre => 'EDGAR QUISPE',
+  :cargo => 'Diseño e Ilustración',
+  :correo => 'edgar@lapost-a.com',
+  :telefono => '979 326 152' )
+
+Post.delete_all
+Post.create(
+  :titulo => 'UN DÍA COMO HOY',
+  :created_at => '28 Ago 2012',
+  :descripcion => 'Microprograma realizado en animación 2D para Cenital S.A.C y RPP.' 
+)
+Post.create(
+  :titulo => 'HUNGRY HOUR',
+  :created_at => '02 Feb 2011',
+  :descripcion => 'Diseño gráfico de banners, banderolas e individuales para Café Café.'
+)
+Post.create(
+  :titulo => 'HUNGRY HOUR',
+  :created_at => '02 Feb 2011',
+  :descripcion => 'Diseño gráfico de banners, banderolas e individuales para Café Café.'
+)
+
