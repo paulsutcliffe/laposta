c31aa63225159c4aa94ba553da3798034a01210f
Ricardo <marcorcp1@hotmail.com>
2012-11-17 19:24:04 -0500

    Added hovers and active to menus, added automatic active class when window scrolling

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,20 +10,70 @@ $(function() {
     return false;
   });
 
+  // menu dinámico
+  var distancia = 200;
+  var bar = $('#menu-dinamico');
+  var top = bar.css('top');
+  $(window).scroll(function() {
+      if($(this).scrollTop() > distancia) {
+          bar.stop().animate({'top' : '0px'}, 500);
+      } else {
+          bar.stop().animate({'top' : top}, 500);
+      }
+  });
 
-      var distancia = 200;
-      var bar = $('#menu-dinamico');
-      var top = bar.css('top');
-      $(window).scroll(function() {
-          if($(this).scrollTop() > distancia) {
-              bar.stop().animate({'top' : '0px'}, 500);
-          } else {
-              bar.stop().animate({'top' : top}, 500);
-          }
-      });
+ 
+  // active menu
+  $('.menu a').click(function () {
+    var $this = $(this);
+    $('.menu a').removeClass('active');
+    $this.addClass('active');
+
+    // click en main menu activa el item en el menu dinamico
+    $('#menu-dinamico .menu li:eq(' + $this.parent().index() + ') a').addClass('active');   
+  })
+
+  // active menu en los botones del portafolio
+  $('#portafolio-botones a').click(function () {
+    var $this = $(this);
+    $('#portafolio-botones a').removeClass('active');
+    $this.addClass('active');
+  })
 
 
+});
 
 
+// automatic addition of active class in menu when scrolling page 
+$(document).scroll(function(){
+    var menu_dinamico = $('#menu-dinamico .menu li a'),
+        laposta = $('#menu-dinamico .menu li:eq(' + 1 +') a'),
+        servicios = $('#menu-dinamico .menu li:eq(' + 2 +') a'),
+        portafolio  = $('#menu-dinamico .menu li:eq(' + 3 +') a'),
+        consultas  = $('#menu-dinamico .menu li:eq(' + 4 +') a');
 
+        laposta_region = $('#laposta').offset().top - $(document).scrollTop();
+        servicios_region = $('#servicios').offset().top - $(document).scrollTop();
+        portafolio_region = $('#postafolio').offset().top - $(document).scrollTop();
+        consultas_region = $('#consultas').offset().top - $(document).scrollTop();
+
+    if (laposta_region  < 200 ){
+        menu_dinamico.removeClass('active');
+        laposta.addClass('active');
+    } 
+
+    if (servicios_region  < 200 ){
+        menu_dinamico.removeClass('active');
+        servicios.addClass('active');
+    }
+ 
+    if (portafolio_region  < 200 ){
+        menu_dinamico.removeClass('active');
+        portafolio.addClass('active');
+    } 
+
+    if (consultas_region  < 200 ){
+        menu_dinamico.removeClass('active');
+        consultas.addClass('active');
+    }
 });
