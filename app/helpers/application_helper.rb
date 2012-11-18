b25b6194257ba60a871494041ab1f3b39352be3c
Ricardo <marcorcp1@hotmail.com>
2012-11-17 20:04:35 -0500

    Added helper to posts paragraphs and external link icon for website links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def paragraph(post, length)
+    "#{truncate(post.descripcion, :length => length)}"
+  end
+
   def agregar_clase_segun_tipo(post)
     "#{post.tipo == 'Diseño y Web'? 'foto' : 'video'}"
   end
@@ -8,14 +12,7 @@ module ApplicationHelper
     post.tipo.gsub(/ /,"").downcase
   end
 
-  def lightbox_si_es_video(post)
 
-    #= link_to '', '01.jpg', 
-    #  :"data-target" => 'flare', 
-     # :"data-flare-gallery" => imagen.post(params[:post_id => post.id]), 
-      #:"data-flare-thumb" => 'thumb01.jpg',
-      #:"data-flare-video" => 'http://www.youtube.com/watch?v=AFA-rOls8YA'
-  end
 
 
 
