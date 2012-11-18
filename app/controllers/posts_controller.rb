5693bd9b2cef4d23d7d97ec4d9b87d61c8cc0356
Ricardo <marcorcp1@hotmail.com>
2012-11-18 00:31:30 -0500

    Added notice js bar, actually not working for errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < InheritedResources::Base
+  before_filter :authenticate_admin!
   def index
     @posts = Post.search(params[:search]).paginate(:per_page => 4, :page => params[:page])
   end
--- app/models/contacto.rb
+++ app/models/contacto.rb
@@ -1,2 +1,7 @@
 class Contacto < ActiveRecord::Base
+  EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+
+  validates :nombre, :presence => true, :length => { :maximum => 50 }
+  validates :email, :presence => true, :length => { :maximum => 100 }, :format => EMAIL_REGEX
+  validates :proyecto, :presence => true
 end
