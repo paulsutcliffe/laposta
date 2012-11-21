2ad6370ac6338312aa4b13663bfdb6f85ccaea9f
Ricardo <marcorcp1@hotmail.com>
2012-11-21 02:50:12 -0500

    Fixed menu, started to style cms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def bodyid
+    @bodyid = params[:controller].parameterize
+  end
+
+  def bodyclass
+    @bodyclass = params[:action].parameterize
+
+    if params[:controller] == 'home'
+      @front = 'front'
+    else
+      @front = 'not-front'
+    end
+
+    if current_admin
+      @logged = 'logged-in'
+    else
+      @logged = 'not-logged-in'
+    end
+
+
+    @bodyclass = @bodyclass + ' ' + @front + ' ' + @logged 
+  end
+
+  helper_method :bodyid
+  helper_method :bodyclass
 end
